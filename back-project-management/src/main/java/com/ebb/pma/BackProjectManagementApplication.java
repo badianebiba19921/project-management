2869d1ae6d5cbf90d91facdfa377ab97d70ee3b9
Elhadji Badiane <badianebiba@gmail.com>
2020-02-18 21:51:46 +0100

    Initialisation du project back

---
+++ back-project-management/src/main/java/com/ebb/pma/BackProjectManagementApplication.java
@@ -0,0 +1,13 @@
+package com.ebb.pma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackProjectManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackProjectManagementApplication.class, args);
+	}
+
+}
